d30ffcee5b3b88fc12b8adb2bc613dbb8175dad7
Todd Selwitz <selwitz.todd@gmail.com>
2023-07-03 14:04:23 -0600

    uvicorn server

--- app/api/main.py
+++ app/api/main.py
@@ -1,1 +1,11 @@
-print("hello world and all who inhabit it")
+# main.py
+import uvicorn
+
+
+async def app(scope, receive, send):
+    return
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
