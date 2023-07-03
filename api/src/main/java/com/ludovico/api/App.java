414db5677030a503b69670ec13db094a4cc8d42e
Todd Selwitz <selwitz.todd@gmail.com>
2023-07-02 18:01:53 -0600

    Removes unnecessary array import

--- api/src/main/java/com/ludovico/api/App.java
+++ api/src/main/java/com/ludovico/api/App.java
@@ -1,7 +1,5 @@
 package com.ludovico.api;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
